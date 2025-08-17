6ec3e822d9b517024219a9bd2f8d8c0f7db3126f
Wiktor <80409661+WitroxHunter@users.noreply.github.com>
2025-08-18 00:16:47 +0200

    minimal changes

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,17 +8,19 @@
         class="relative text-center flex flex-col items-center justify-center"
       >
         <h1 class="text-7xl font-bold mb-4">Spin'n Sound</h1>
-        <p class="text-xl mb-8">Your choice for the best vinyl & CD records.</p>
+        <p class="text-xl mb-8 text-[#c1c1c1]">
+          Your choice for the best vinyl & CD records.
+        </p>
 
         <!-- Dwa przyciski obok siebie -->
         <div class="flex gap-4">
           <button
-            class="p-4 bg-[#633131] rounded-lg hover:bg-[#633131] transition"
+            class="cursor-pointer py-4 px-8 bg-[#633131] border-[#633131] hover:bg-[#582c2c] border-2 rounded-lg transition"
           >
             Shop now!
           </button>
           <button
-            class="p-4 bg-[#ffffff2d] rounded-lg hover:bg-[#ffffff26] transition"
+            class="cursor-pointer p-4 border-2 border-[#ffffff2d] bg-[#ffffff0a] rounded-lg hover:bg-[#ffffff26] transition"
           >
             Contact Us
           </button>
