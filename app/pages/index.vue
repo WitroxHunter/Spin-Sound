3abc5b905e7e3c4c06542179c03e4cbd4c053cd4
Wiktor <80409661+WitroxHunter@users.noreply.github.com>
2025-11-12 01:47:53 +0100

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -138,41 +138,27 @@
 
         <div class="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-8">
           <div
-            v-for="category in categories"
-            :key="category.id"
-            class="group cursor-pointer w-full"
+            v-for="product in paginatedProducts"
+            :key="product?.id || product?._id"
+            v-if="product"
+            class="bg-[#2A1F1F] cursor-pointer p-4 rounded-xl shadow hover:scale-105 transition transform flex flex-col"
           >
-            <NuxtLink :to="`/products/${category.link}`">
-              <div
-                class="relative w-full aspect-square rounded-lg overflow-hidden border-2 border-[#633131] hover:scale-105 transition-all duration-300 hover:shadow-2xl hover:shadow-[#633131]/20"
-              >
-                <!-- Background image -->
-                <img
-                  :src="`/images/${category.img}`"
-                  :alt="category.name"
-                  class="absolute inset-0 w-full h-full object-cover"
-                />
-
-                <!-- Gradient overlay -->
-                <div
-                  class="absolute inset-0 bg-gradient-to-b from-[#633131]/70 to-[#1d1616]/90"
-                ></div>
-
-                <!-- Text content at the bottom -->
-                <div class="absolute bottom-0 w-full p-4 text-center">
-                  <h3 class="text-white font-semibold text-xl">
-                    {{ category.name }}
-                  </h3>
-                  <p class="text-[#c1c1c1] text-sm mb-2">
-                    {{ category.description }}
-                  </p>
-                  <div
-                    class="bg-[#ffffff1a] rounded-lg p-2 text-xs text-white inline-block"
-                  >
-                    {{ category.count }} products
-                  </div>
-                </div>
-              </div>
+            <NuxtLink
+              v-if="product?.id"
+              :to="`/product/${slugify(
+                product.id + '-' + product.name + '-' + product.category
+              )}`"
+            >
+              <img
+                :src="product.image"
+                :alt="product.name"
+                class="rounded-lg w-full object-cover mb-4"
+              />
+              <h3 class="text-xl font-bold">{{ product.name }}</h3>
+              <p class="text-gray-400">{{ product.artist }}</p>
+              <p class="mt-auto text-lg font-semibold">
+                {{ product.price }} zł
+              </p>
             </NuxtLink>
           </div>
         </div>
